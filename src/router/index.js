b04fca0550b7ab94c1231c73274e5b65d359892b
Bios <zhangbiaoFT@163.com>
2017-09-19 01:14:03 +0800

    router init

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import Hello from '@/components/Hello'
 import GoodsList from '@/views/GoodsList'
-import Title from '@/views/title'
-import Img from '@/views/img'
-import Cart from '@/views/cart'
 
 Vue.use(Router)
 
@@ -12,18 +9,9 @@ export default new Router({
   mode: 'history', // history or hash
   routes: [
     {
-      path: '/goods',
+      path: '/',
       name: 'GoodsList',
-      components: {
-        default: GoodsList,
-        title: Title,
-        img: Img
-      }
-    },
-    {
-      path: '/cart',
-      name: 'cart',
-      component: Cart
+      component: GoodsList
     }
   ]
 })
