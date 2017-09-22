1a76535e12b45dd365c6d9378c89d4168aa899d2
Bios <zhangbiaoFT@163.com>
2017-09-22 21:30:18 +0800

    修改goodsmodel

--- server/models/goods.js
+++ server/models/goods.js
@@ -1,10 +1,12 @@
 var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 var produtSchema = new Schema({
-    "productId": String,
-    "productName": String,
-    "salePrice": Number,
-    "productImage": String
+  "productId":{type:String},
+  "productName":String,
+  "salePrice":Number,
+  "checked":String,
+  "productNum":Number,
+  "productImage":String
 })
 
 module.exports = mongoose.model('Good', produtSchema)
