bd29ed154c02d799da53172846a3312929ae61b8
Mario <zhangbiaoFT@163.com>
2017-09-22 14:01:53 +0800

    购物车未完

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 // import Hello from '@/components/Hello'
 import GoodsList from '@/views/GoodsList'
+import Cart from '@/views/Cart'
 
 Vue.use(Router)
 
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'GoodsList',
       component: GoodsList
+    },
+    {
+      path: '/cart',
+      name: 'Cart',
+      component: Cart
     }
   ]
 })
