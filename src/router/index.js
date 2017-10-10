b0875c4dcfa89015db8338fa3f54fc224aae275f
Mario <zhangbiaoFT@163.com>
2017-10-10 10:43:16 +0800

    订单生成成功

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import GoodsList from '@/views/GoodsList'
 import Cart from '@/views/Cart'
 import Address from '@/views/Address'
 import OrderConfirm from '@/views/OrderConfirm'
+import OrderSuccess from '@/views/OrderSuccess'
 
 Vue.use(Router)
 
@@ -30,6 +31,11 @@ export default new Router({
       path: '/orderConfirm',
       name: 'OrderConfirm',
       component:OrderConfirm
+    },
+    {
+      path: '/orderSuccess',
+      name: 'OrderSuccess',
+      component:OrderSuccess
     }
   ]
 })
